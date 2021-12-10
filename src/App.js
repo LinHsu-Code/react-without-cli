887d46d0dbdf290eaa50f3494391ecc1294e3d3c
Lin <gxl0216@163.com>
2021-12-10 16:44:00 +0800

    finished

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ import TaskList from "./components/TaskList";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { tasks: [111, 111, 1223] };
+    this.state = { tasks: [] };
     this.handleAddTask = this.handleAddTask.bind(this);
   }
 
-  handleAddTask() {}
+  handleAddTask(newTask) {
+    const tasks = [...this.state.tasks, newTask];
+    this.setState({
+      tasks: tasks,
+    });
+  }
 
   render() {
     return (
       <div className="task-container">
-        <AddTask />
+        <AddTask onAddTask={this.handleAddTask} />
         <TaskList tasks={this.state.tasks} />
       </div>
     );
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,10 +1,53 @@
 //import React from "react";
-const AddTask = (props) => {
-  return (
-    <div className="add-task-wrapper">
-      <input id="task" name="task" type="text" placeholder="new task" />
-      <button type="submit">Submit</button>
-    </div>
-  );
-};
+// const AddTask = (props) => {
+//   const handleAddTask = (e) => {
+//     props.onAddTask("nfksdjfklsd");
+//   };
+//   return (
+//     <div className="add-task-wrapper">
+//       <input id="task" name="task" type="text" placeholder="new task" />
+//       <button type="submit" onClick={handleAddTask}>
+//         Submit
+//       </button>
+//     </div>
+//   );
+// };
+// export default AddTask;
+import { Component } from "react";
+class AddTask extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { inputTest: "" };
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleAddTask = this.handleAddTask.bind(this);
+  }
+  handleInputChange(e) {
+    this.setState({ inputTest: e.target.value });
+  }
+  handleAddTask(newTask) {
+    this.props.onAddTask(newTask);
+    this.setState({ inputTest: "" });
+  }
+
+  render() {
+    return (
+      <div className="add-task-wrapper">
+        <input
+          id="task"
+          name="task"
+          type="text"
+          placeholder="new task"
+          value={this.state.inputTest}
+          onChange={(e) => this.handleInputChange(e)}
+        />
+        <button
+          type="submit"
+          onClick={(e) => this.handleAddTask(this.state.inputTest, e)}
+        >
+          Submit
+        </button>
+      </div>
+    );
+  }
+}
 export default AddTask;
